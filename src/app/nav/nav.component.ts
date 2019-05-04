a2cf9c47eb54affab7035a9a00af310fa00a4ebf
Leona Ponce <lpon6dev@gmail.com>
2019-05-04 14:43:10 -0400

    Sass added

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  appTitle: string = 'Futurism Twitter Showcase App'
+  
   constructor() { }
 
   ngOnInit() {
